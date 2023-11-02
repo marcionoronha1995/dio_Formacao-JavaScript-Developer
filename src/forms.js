964bce3834af0b842dc742295347ac32e86bae9e
marcionoronha1995 <123603516+marcionoronha1995@users.noreply.github.com>
2023-11-02 11:25:48 -0300

    forms javascript

---
+++ src/forms.js
@@ -0,0 +1,12 @@
+
+
+function minha_Funcao() {
+    const message = 'Hello world Teste'; // Try edit me
+
+// Update header text
+document.querySelector('texto_A').innerHTML = message;;
+
+// Log to console
+console.log(message);
+
+}
