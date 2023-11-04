1a72ef739270940902293da11bb4643f1713c397
marcionoronha1995 <123603516+marcionoronha1995@users.noreply.github.com>
2023-11-04 08:48:42 -0300

    forms

--- src/forms.js
+++ src/forms.js
@@ -4,9 +4,19 @@ function minha_Funcao() {
     const message = 'Hello world Teste'; // Try edit me
 
 // Update header text
-document.querySelector('texto_A').innerHTML = message;;
+document.querySelector('texto_A').innerHTML = message;
 
 // Log to console
 console.log(message);
 
+};
+
+function escolha_Carro() {
+
+    var message = document.getElementById('cars').value;
+    document.querySelectorAll('escolha_Carro').innerHTML = message;
+    console.log("desentro de escolha o carro")
+
+
 }
+
---
+++ src/tela_Alerta.js
@@ -0,0 +1,5 @@
+function tela_Alerta() {
+    let carro_Escolhido = document.getElementById('cars').value;
+    console.log("Tela de arleta");
+    alert(`Tela de Alerta, o carro escolhido foi : ${carro_Escolhido}`);
+}
